1cb1d18cda4697eb92f30a31815ab60d3300da4b
Launch Academy <launchacademy@Launchs-MacBook-Pro.local>
2022-06-21 17:42:23 -0400

    finished code

--- game.rb
+++ game.rb
@@ -2,5 +2,41 @@ require 'pry'
 
 # Note: we only need `require_relative` if we end up calling a class by name in our file's logic. As such, you may have to add more `require_relative` statements accordingly.
 require_relative "lib/deck"
+require_relative "lib/hand"
 
-# GAME START
+#GAME START
+
+# Your game logic here.
+deck = Deck.new
+
+puts "GAME START!"
+puts "There are #{deck.cards.length} cards in the deck."
+
+# binding.pry
+player_1 = Hand.new(deck.deal)
+
+
+player_2_cards = deck.deal
+player_2 = Hand.new(player_2_cards)
+
+player_1_all_cards = []
+player_1.cards.each do |card|
+    string = "#{card.rank} #{card.suit}"
+    player_1_all_cards.push(string)
+end
+
+
+puts "Player 1 was dealt #{player_1_all_cards.join(", ")}"
+
+# binding.pry
+
+puts "Player 1 hand's value: #{player_1.value}"
+puts "Player 2 hand's value: #{player_2.value}"
+
+if player_1.value > player_2.value
+    puts "Player 1 is the winner!"
+elsif player_2.value > player_1.value
+    puts "Player 2 is the winner!"
+else
+    puts "TIE!"
+end
--- lib/hand.rb
+++ lib/hand.rb
@@ -1,1 +1,18 @@
-# Your code here
+class Hand
+    attr_reader :cards
+    
+    def initialize(card_objects)
+        @cards = card_objects
+    end
+
+    def value
+        total_value = 0
+
+        @cards.each do |card|
+            # binding.pry
+            total_value += card.value
+        end
+
+        return total_value
+    end
+end
